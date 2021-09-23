2bbaca479371d32279c6c662b8b2f5a3cfcb85ca
Sean <simpson.sean@outlook.com>
2021-09-23 11:51:00 -0700

    WORKDONE: Inital ACP; Rendering Red block on page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,24 +2,24 @@ import React, { useState } from 'react';
 
 
 const useRecord = (init) => {
-     const [ current, setCurrent ] = useState(init);
+     const [ currentColor, setCurrentColor ] = useState(init);
     //  const { record, useRecord } = useState()
     //  const { undo, setUndo } = useState();
     //  const { redo, setRedo } = useState();
 
-    return {current}
+    return {currentColor};
     
 };
 
 function App() {
-  const { current } = useRecord('#FF0000');
+  const { currentColor } = useRecord('#FF0000');
 
   return (
     <>
       <button>undo</button>
       <button>redo</button>
-      <input type="color" value={current} />
-      <div style={{ backgroundColor: current, width: '10rem', height: '10rem' }}></div>
+      <input type="color" value={currentColor} />
+      <div style={{ backgroundColor: currentColor, width: '10rem', height: '10rem' }}></div>
     </>
   )
  }
