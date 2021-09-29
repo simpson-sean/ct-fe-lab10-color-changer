005fa953a253b0d9ac4f4ca3c6a6dc7d0e21e483
Sean <simpson.sean@outlook.com>
2021-09-29 10:21:27 -0700

    WORKDONE: Passing functionality test

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -7,10 +7,10 @@ export default function App() {
 
   return (
     <>
-      <button onClick={undoColor}>undo</button>
-      <button onClick={redoColor}>redo</button>
-      <input type="color" value={currentColor} onChange={({ target }) => record(target.value)} />
-      <div style={{ backgroundColor: currentColor, width: '10rem', height: '10rem' }}></div>
+      <button onClick={undoColor} aria-label='undo-button'>undo</button>
+      <button onClick={redoColor} aria-label='redo-button'>redo</button>
+      <input type="color" aria-label='col-selector'value={currentColor} onChange={({ target }) => record(target.value)} />
+      <div style={{ backgroundColor: currentColor, width: '10rem', height: '10rem' }} data-testid='render-div'></div>
     </>
   )
 }
